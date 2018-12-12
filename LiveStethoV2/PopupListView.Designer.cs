4632038ea21a0144b117c819cc49346e49f4f960
Pratik Agrawal <pratik.agrawal@bresotec.com>
2018-12-11 23:39:56 -0500

    analyze label added

--- LiveStethoV2/PopupListView.Designer.cs
+++ LiveStethoV2/PopupListView.Designer.cs
@@ -71,10 +71,12 @@
             // 
             // EnableAnalyze
             // 
+            this.EnableAnalyze.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EnableAnalyze.Location = new System.Drawing.Point(12, 231);
             this.EnableAnalyze.Name = "EnableAnalyze";
-            this.EnableAnalyze.Size = new System.Drawing.Size(104, 24);
+            this.EnableAnalyze.Size = new System.Drawing.Size(86, 34);
             this.EnableAnalyze.TabIndex = 1;
+            this.EnableAnalyze.Text = "Analyze";
             // 
             // PopupListView
             // 
@@ -83,6 +85,7 @@
             this.ClientSize = new System.Drawing.Size(411, 292);
             this.Controls.Add(this.SoundDataList);
             this.Controls.Add(this.EnableAnalyze);
+            this.Name = "PopupListView";
             this.Text = "Recorded Sound Files";
             this.Load += new System.EventHandler(this.PopupListViewLoad);
             this.ResumeLayout(false);
